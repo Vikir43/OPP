97726f83cb7624c25118ca8926805978b5861cba
Viki <viki333@list.ru>
2023-06-26 12:00:27 +0300

    New  fail

---
+++ Homework_3/Student.java
@@ -0,0 +1,21 @@
+package Homework_3;
+
+import lombok.Data;
+
+//@Getter
+//@AllArgsConstructor
+
+//@Setter
+//@ToString
+//@EqualsAndHashCode
+@Data
+public class Student implements Comparable<Student> {
+
+    private final Long id;
+    private final String fullName;
+
+    @Override
+    public int compareTo(Student o) {
+        return fullName.compareTo(o.fullName);
+    }
+}
