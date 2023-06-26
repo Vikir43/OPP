ab0fbf0a9dd323f01822420fe8d1f82f8696aee8
Viki <viki333@list.ru>
2023-06-26 12:33:17 +0300

    New  fail

---
+++ Homework_4/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package Homework_4.repository;
+
+import Homework_4.model.User;
+
+import java.util.List;
+
+public interface UserRepository<T extends User> {
+
+    List<T> getAll();
+    void add(T t);
+    void remove(String name);
+    Long getMaxId();
+
+}
+
---
+++ Homework_4/view/UserView.java
@@ -0,0 +1,11 @@
+package Homework_4.view;
+
+import Homework_4.model.User;
+
+public interface UserView<T extends User> {
+
+    void sendOnConsole(String sortType);  // GET request
+    void create(String fullName, Integer age, String phoneNumber);  // POST request
+    void remove(String fullName);  // DELETE request
+    void edit(String fullName, Integer age, String phoneNumber);  // PUT request
+}
